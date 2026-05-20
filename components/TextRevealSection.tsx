d443e36e16203b30de2dec71fbc13a95ef42e7ed
Vasant Katara <vasant.quicknish@gmail.com>
2026-05-20 12:50:31 +0530

    Fixed GTM code and change text

--- components/TextRevealSection.tsx
+++ components/TextRevealSection.tsx
@@ -3,7 +3,7 @@ import ScrollReveal from "./Reusable/ScrollReveal";
 
 const TextRevealSection = () => {
   const TEXT =
-    "Blue & Blues is a quiet luxury leather goods brand rooted in Italian design heritage and crafted through Indian artistry. Founded in Milan, our philosophy blends purposeful design with timeless elegance—where form follows meaning, and every detail serves a reason. Created in small batches using responsibly sourced materials, our pieces are designed to move effortlessly through modern life, evolving with you and developing a character uniquely your own. Blue & Blues creates companions for life, not products for moments.";
+    "Blue & Blues was born from the belief that the things we carry everyday should be worth carrying forever. Crafted by Indian artisans for generations. Made in small batches, with care, precision and meaning.";
 
   return (
     <div data-light-bg className=" py-20  px-6">
